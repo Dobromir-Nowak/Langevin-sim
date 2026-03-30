48f148ff5cf8bebfc62a6d7b69e6e6b57662b4e9
DobromirN <dobromir.nowak@etu.unige.ch>
2026-03-30 17:37:08 +0200

    wip

--- scripts/run_cylinder3d_debugging.py
+++ scripts/run_cylinder3d_debugging.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from langevin_sim.utils import load_config, plot_hist_rho, plot_hist_z, const_initial_conditions
 from langevin_sim.langevin import Langevin_sim
-from langevin_sim.geometry import Cylinder3D, random_initial_conditions_Cylinder3D
+from langevin_sim.geometry import Cylinder3D
 
 # Load plot style
 parent_dir = Path(__file__).parent.parent
@@ -22,12 +22,19 @@ f_fn = f
 I_fn = I_identity
 
 
-file_name = "config_Cylinder3D"
+file_name = "config_Cylinder3D_debugging"
 config = load_config(file_name=file_name)
 
 
 # Initial conditions
-r_init, n_init = random_initial_conditions_Cylinder3D(config)
+r_const = np.array([0,0,0.9],dtype=float)
+n_const = np.array([0,0,1],dtype=float)
+
+# r_const = 0.95*np.array([6,8,1],dtype=float)
+# n_const = np.sqrt(2)*np.array([0.6,0.8,0],dtype=float) + np.sqrt(2)*np.array([0,0,1])
+
+
+r_init, n_init = const_initial_conditions(config=config, r_const=r_const, n_const=n_const)
 
 # Run simulation
 sim = Langevin_sim(config,I_fn=I_fn, f_fn=f_fn)
@@ -42,6 +49,7 @@ x, y, z = r[-1,0,:], r[-1,1,:], r[-1,2,:]
 rho = np.sqrt(x**2+y**2)
 
 # Plotting
-sim.plot_trajectories(aspect_ratio = [1,1,1])
 # plot_hist_rho(rho)
 # plot_hist_z(z)
+# sim.plot_trajectories(aspect_ratio = [1,1,1])
+sim.plot_trajectories(aspect_ratio = [1,1,1], show_cylinder=True, config=config)
