93ae1a8b6ba92e691934ae2d1d0b554888f45b8e
DobromirN <dobromir.nowak@etu.unige.ch>
2026-05-13 13:20:00 +0200

    wip plotting

--- scripts/run_cuboid_lin_current_new_bc.py
+++ scripts/run_cuboid_lin_current_new_bc.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from langevin_sim.utils.compute import I_identity, F, const_initial_conditions, const_initial_conditions_split, make_linear_grad_beam
 from langevin_sim.utils.other import load_config
-from langevin_sim.plotting.plots import plot_hist
+from langevin_sim.plotting.plots import plot_hist, plot_hist_ax, PlotCollector
 from langevin_sim.io.results import ResultsManager
 
 from langevin_sim.physics.langevin import Langevin_sim
@@ -22,7 +22,18 @@ config = load_config(config_path=config_path)
 alpha = 0.28
 
 f_fn = F
-I_fn = make_linear_grad_beam(config=config)
+# I_fn = make_linear_grad_beam(config=config)
+
+sigma_beam = 30
+x0 = 50
+def I_fn(r):
+    x, y = r[0,:], r[1,:]
+    return np.exp(-((x-x0)**2)/(2*sigma_beam**2))[None,:]
+
+
+
+
+
 
 rm = ResultsManager(config_path=config_path, tag="cuboid_lin")
 
@@ -36,7 +47,7 @@ r_init, n_init = geometry.random_initial_conditions()
 # r_init, n_init = const_initial_conditions(config=config, r_const=r_const, n_const=n_const)
 
 # Run simulation 
-save_every = 1000
+save_every = 100
 sim = Langevin_sim(config,I_fn=I_fn,f_fn=f_fn,r0=r_init, n0=n_init, geometry=geometry)
 results = sim.run(save_every=save_every)
 # sim.plot_trajectories()
@@ -46,58 +57,126 @@ x, y, z = r[-1,0,:], r[-1,1,:], r[-1,2,:]
 nx, ny, nz = n[-1,0,:], n[-1,1,:], n[-1,2,:]
 
 
+
+
+
 # Hist
-
-
-
 r, n = results["r"], results["n"]
 x, y, z = r[-1,0,:], r[-1,1,:], r[-1,2,:]
 z_some = z[z>0.99*config["Lz"]]
 
-# Plotting
-plot_hist(z, axis_label="z", bins=20)
-plot_hist(z_some, axis_label="z_some", bins=20)
+# plot_hist(z, axis_label="z", bins=20)
+# plot_hist(z_some, axis_label="z_some", bins=20)
+
+
+def plot_current_ax(ax, x, z, nx, nz, config, bins_x=20, bins_z=20):
+
+    Lx = config["Lx"]
+    Lz = config["Lz"]
+
+    dx = Lx / bins_x
+    dz = Lz / bins_z
+
+    ix = (x / dx).astype(int)
+    iz = (z / dz).astype(int)
+
+    Jx = np.zeros((bins_x, bins_z))
+    Jz = np.zeros((bins_x, bins_z))
+
+    np.add.at(Jx, (ix, iz), nx)
+    np.add.at(Jz, (ix, iz), nz)
+
+    Jx /= dx
+    Jz /= dz
+
+    x_centers = (np.arange(bins_x) + 0.5) * dx
+    z_centers = (np.arange(bins_z) + 0.5) * dz
+
+    X, Z = np.meshgrid(x_centers, z_centers, indexing="ij")
+
+    ax.quiver(X, Z, Jx, Jz)
+
+    ax.set_xlim(0, Lx)
+    ax.set_ylim(0, Lz)
+
+
+def plot_density_ax(ax, x, z, config, bins_x=20, bins_z=20, cmap="viridis"):
+    Lx = config["Lx"]
+    Lz = config["Lz"]
+
+    dx = Lx / bins_x
+    dz = Lz / bins_z
+
+    ix = (x / dx).astype(int)
+    iz = (z / dz).astype(int)
+
+    # optional safety against boundary overflow
+    ix = np.clip(ix, 0, bins_x - 1)
+    iz = np.clip(iz, 0, bins_z - 1)
+
+    rho = np.zeros((bins_x, bins_z))
+
+    np.add.at(rho, (ix, iz), 1 / (dx * dz))
+
+    im = ax.imshow(
+        rho.T,
+        origin="lower",
+        extent=[0, Lx, 0, Lz],
+        aspect="auto",
+        cmap=cmap
+    )
+
+    ax.set_xlim(0, Lx)
+    ax.set_ylim(0, Lz)
+
+
+pc = PlotCollector()
+pc.add(plot_current_ax, x, z, nx, nz, config, bins_x=20, bins_z=20)
+pc.add(plot_density_ax, x, z, config)
+pc.add(plot_hist_ax, z, axis_label="z", bins=20)
+pc.add(plot_hist_ax, x, axis_label="x", bins=20)
+rm.save_plot(pc.render(), name= f"joint_fig_t={config["Nt"]*config["dt"]}")
 
 
 
-# 2D currents xz
 
-bins_x = bins_z = 20
+# # 2D currents xz
+# bins_x = bins_z = 20
 
-Lx = config["Lx"]
-Lz = config["Lz"]
-dx = Lx/bins_x
-dz = Lz/bins_z
+# Lx = config["Lx"]
+# Lz = config["Lz"]
+# dx = Lx/bins_x
+# dz = Lz/bins_z
 
-ix = (x/dx).astype(int) # indices of x bins
-iz = (z/dz).astype(int)
+# ix = (x/dx).astype(int) # indices of x bins
+# iz = (z/dz).astype(int)
 
-Jx = np.zeros((bins_x,bins_z))
-Jz = np.zeros((bins_x, bins_z))
-np.add.at(Jx, (ix, iz), nx)
-np.add.at(Jz, (ix, iz), nz)
-Jx /= dx
-Jz /= dz
+# Jx = np.zeros((bins_x,bins_z))
+# Jz = np.zeros((bins_x, bins_z))
+# np.add.at(Jx, (ix, iz), nx)
+# np.add.at(Jz, (ix, iz), nz)
+# Jx /= dx
+# Jz /= dz
 
-x_centers = (np.arange(bins_x) + 1/2)*dx
-y_centers = (np.arange(bins_z) + 1/2)*dz
-X, Z = np.meshgrid(x_centers, y_centers, indexing='ij')
+# x_centers = (np.arange(bins_x) + 1/2)*dx
+# y_centers = (np.arange(bins_z) + 1/2)*dz
+# X, Z = np.meshgrid(x_centers, y_centers, indexing='ij')
 
-plt.quiver(X, Z, Jx, Jz)
-plt.show()
+# plt.quiver(X, Z, Jx, Jz)
+# plt.show()
 
-# Density (rho) xz
-rho = np.zeros((bins_x,bins_z))
-np.add.at(rho, (ix, iz), 1/(dx*dz))   # TODO check normalization when it becomes relevant
+# # Density (rho) xz
+# rho = np.zeros((bins_x,bins_z))
+# np.add.at(rho, (ix, iz), 1/(dx*dz))   # TODO check normalization when it becomes relevant
 
-plt.imshow(
-    rho.T,
-    origin='lower',
-    extent=[0, Lx, 0, Lz],
-    aspect='auto'
-)
-plt.colorbar(label='density')
-plt.show()
+# plt.imshow(
+#     rho.T,
+#     origin='lower',
+#     extent=[0, Lx, 0, Lz],
+#     aspect='auto'
+# )
+# plt.colorbar(label='density')
+# plt.show()
 
 
 # # 2D currents xy
